4828df8a0036f17724f11818c5c00d7e1ce92ca7
Alekey Upirvitskiy <aleks5d@mail.ru>
2023-02-12 18:43:39 +0300

    RickRoll

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const ALCHEMY_API_KEY = "newer gonna give you up";
+const GOERLI_PRIVATE_KEY = "Never gonna let you down";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
 };
